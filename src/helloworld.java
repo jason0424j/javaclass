8197178978aa271c7973f3668a7743d7b33de116
jason chou <jason chou>
2016-10-20 17:25:48 +0800

    journal.txt

---
+++ src/helloworld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jason chou on 2016/10/20.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class helloworld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}

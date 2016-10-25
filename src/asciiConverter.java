beede1f3360b88277ca68327ab9d04759d7a3727
jason chou <jason chou>
2016-10-25 17:30:56 +0800

    journal.txt

---
+++ src/asciiConverter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/**
+ * import java.util.Sca
+ * Created by jason chou on 2016/10/25.
+ * this program converts a number given by the user into ascii code
+ */
+public class asciiConverter {
+
+        public static void main(String[] args)
+        {
+
+        }
+    }
---
+++ src/charnumbers.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jason chou on 2016/10/25.
+ */
+public class charnumbers {
+    public static void main(String[] args)
+    {
+        char letter;
+        letter = 37;
+        System.out.println(letter);
+    }
+}
